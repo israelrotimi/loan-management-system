cd0f8210e0ef07c9f1e599bdb827d37039845d4a
Israel Rotimi <izzyrotimi@gmail.com>
2025-02-11 09:40:09 +0100

    feat: added UserOptions component and implemented it's functionality

---
+++ src/utils/apiUtils.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = "https://run.mocky.io/v3/7464a859-60a9-439e-b871-1eb9e9e0121c";
+
+
+export interface User{
+  id: number,
+  organization: string,
+  fullname: string,
+  username: string,
+  email: string,
+  phone_no: string,
+  date_joined: string,
+  status: string,
+  loan_amount: 10,
+  savings: 1
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const response = await axios.get(API_URL)
+  const data = response.data;
+  return data;
+}
+
+
+
+export { fetchUsers }
